c249c8ac0d472cc8df841b87a8ca12a46119ee14
Rakesh <rakeshmtawargeri07@gmail.com>
2026-08-25 01:16:11 +0530

    Add pattern printing logic in pattern.java
    Implement a pattern printing program in Java.

---
+++ pattern.java
@@ -0,0 +1,31 @@
+public class pattern{
+    public static void main(String[]args){
+int n=4;
+
+for(int i=1;i<=n;i++){
+    for(int j=1;j<=6;j++){
+        if(i==1||i==n){
+            System.out.print("*");
+
+        }
+        else{
+            if(j==1){
+                System.out.print("*");
+            }
+            else if(j==n){
+                System.out.print("*");
+            }
+            else{
+                System.out.print("  ");
+            }
+        }
+    }
+    System.out.println();
+
+}
+
+
+
+
+    }
+}
