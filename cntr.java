30a675354591caf47f8a7e6ce80ee3fa61dfc9d5
Rakesh <rakeshmtawargeri07@gmail.com>
2026-08-25 20:51:38 +0530

    1st commit

---
+++ cntr.java
@@ -0,0 +1,23 @@
+public class cntr{
+
+    public int age;
+    public int salary;
+    public String name;
+    public String company;
+
+
+public cntr(int age,int salary,String name,String company){
+    this.age=age;
+    this.salary=salary;
+    this.name=name;
+    this.company=company;
+}
+
+public  void sleep(){
+    System.out.println("he is slleping");
+}
+
+public  void drink(){
+    System.out.println("he is drinking");
+}
+}
---
+++ obj.java
@@ -0,0 +1,12 @@
+public class obj{
+
+    public static void main(String[]args){
+
+        cntr a=new cntr(28,50000,"rakesh","microsoft");
+        System.out.println(a.salary);
+        System.out.println(a.name);
+
+        a.drink();
+
+    }
+}
