99d520422d0c4c2a56db71ac25bfb5532db70c2b
Rakesh <rakeshmtawargeri07@gmail.com>
2026-08-28 02:04:18 +0530

    1st commit

--- reversestring.java
+++ reversestring.java
@@ -1,16 +1,3 @@
 public class reversestring {
-
-    public static String reverse(String str){
-        String reverse="";
-        for(int i=str.length()-1;i>=0;i--){
-            char ch=str.charAt(i);
-            reverse=reverse+ch;
-        }
-        return reverse;
-    }
-    public static void main(String[] args){
-        String str="ramesh";
-        System.out.println(reverse(str));
-
-    }
+    
 }
---
+++ words.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class words{
+
+    public static int noofwords(String s){
+
+    int count=0;
+    for(int i=0;i<s.length();i++){
+        if(s.charAt(i)==' '){
+            count++;
+        }
+    }
+    return count + 1;
+
+    }
+
+    public static void main(String [ ] args){
+        Scanner sc=new Scanner(System.in);
+        String s="how are u";
+        System.out.println(noofwords(s));
+
+
+
+
+
+    }
+}
