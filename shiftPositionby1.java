ba30acf47b95b4a659d8b312ad9c06d06355bf35
Rakesh <rakeshmtawargeri07@gmail.com>
2026-09-24 00:16:42 +0530

    1st commit

---
+++ shiftPositionby1.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+//right shift elemnts by one positon
+class shiftPositionby1{
+    public static void main(String [] args){
+        Scanner sc = new Scanner(System.in);
+        int n=sc.nextInt();
+
+        int arr[]=new int[n];
+
+        for(int i=0;i<arr.length;i++){
+            arr[i]=sc.nextInt();
+        }
+
+        int temp=arr[n-1];
+        for(int i=n-1;i>0;i--){
+            arr[i]=arr[i-1];
+        }
+        arr[0]=temp;
+       
+    
+    for(int i=0;i<arr.length;i++){
+        System.out.println(arr[i]);
+    }
+}
+}
