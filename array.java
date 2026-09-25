c44161c8e5f72ed77a750881c437b1cb7abbb6da
Rakesh <rakeshmtawargeri07@gmail.com>
2026-09-25 23:49:19 +0530

    Update array.java

--- array.java
+++ array.java
@@ -9,6 +9,7 @@ public class array {
         for (int i = 0; i < number.length; i++) {
             number[i] = sc.nextInt();
         }
+        
         int x = sc.nextInt();
         for (int i = 0; i < number.length; i++) {
             if (number[i] == x) {
